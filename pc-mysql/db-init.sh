12cb0be52d4371401de65eccd94d74a9b0c2cb45
Amir Katz <akrk1986@gmail.com>
2018-01-22 20:54:22 +0200

    init now working, checkpointing

--- pc-mysql/db-init.sh
+++ pc-mysql/db-init.sh
@@ -6,27 +6,26 @@ export DB_NAME=petclinic
 export DB_HOST=127.0.0.1
 export ORIG_DB=test
 
-echo "copy config file"
+echo "1 - copy config file"
 cp .my.cnf /root
 chmod 600 /root/.my.cnf
 
-echo "Start MySQL"
+echo "2 - Start MySQL"
 /etc/init.d/mysql start
 
-echo "Create DB schema"
-CMD="mysql -h ${DB_HOST} ${ORIG_DB} <schema.sql"
+echo "3 - Create DB schema"
+CMD="mysql -v -h ${DB_HOST} ${ORIG_DB} <schema.sql"
 echo "CMD=${CMD}"
 echo "================"
-${CMD}
+#${CMD}
 
 if [ $? -ne 0 ]; then
     echo "FAILED!!"
     exit 1
 fi
 
-echo "Insert/update DB data"
+echo "4 - Insert/update DB data"
 CMD="mysql -h 127.0.0.1 ${DB_NAME} <data.sql"
 echo "CMD=${CMD}"
 echo "================"
-${CMD}
-
+#${CMD}
