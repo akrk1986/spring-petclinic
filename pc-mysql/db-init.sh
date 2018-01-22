0a7ca66df075502cf2ae18251692cc745c46da9b
Amir Katz <akrk1986@gmail.com>
2018-01-22 22:34:26 +0200

    moved db init files to /docker-entrypoint-initdb.d/, do not use shell script

--- pc-mysql/db-init.sh
+++ pc-mysql/db-init.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-export DB_USER=root
-export DB_PASS=root
 export DB_NAME=petclinic
 export DB_HOST=127.0.0.1
 export ORIG_DB=test
 
-echo "1 - copy config file"
-cp .my.cnf /root
-chmod 600 /root/.my.cnf
+#echo "1 - copy config file"
+#cp .my.cnf /root
+#chmod 600 /root/.my.cnf
 
-echo "2 - Start MySQL"
-/etc/init.d/mysql start
+#echo "2 - Start MySQL"
+#/etc/init.d/mysql start
 
 echo "3 - Create DB schema"
 CMD="mysql -v -h ${DB_HOST} ${ORIG_DB} <schema.sql"
 echo "CMD=${CMD}"
 echo "================"
-#${CMD}
+${CMD}
 
 if [ $? -ne 0 ]; then
     echo "FAILED!!"
@@ -25,7 +23,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "4 - Insert/update DB data"
-CMD="mysql -h 127.0.0.1 ${DB_NAME} <data.sql"
+CMD="mysql -h ${DB_HOST} ${DB_NAME} <data.sql"
 echo "CMD=${CMD}"
 echo "================"
-#${CMD}
+${CMD}
