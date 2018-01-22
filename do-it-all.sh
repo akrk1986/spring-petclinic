14e5832c5bbdc9a371d0c741e10ea2e717a36b7c
Amir Katz <akrk1986@gmail.com>
2018-01-22 23:47:19 +0200

    Almost done

---
+++ do-it-all.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# assuming docker is running
+
+function stepper {
+    echo "=============================="
+    echo $1
+    echo "=============================="
+}
+    
+function check_rc {
+    if [ $1 -ne 0 ]; then
+        echo "Command failed for $2, rc=$1"
+        exit 1
+    fi
+}
+
+stepper "Building spring-boot app #1"
+docker build -t pet-clinic-01 .
+check_rc $? "Building container 1"
+
+stepper "Building spring-boot app #2"
+docker build -t pet-clinic-02 .
+check_rc $? "Building container 2"
+
+stepper "Spinning up docker-compose"
+
+docker-compose down
+docker-compose up -d
+check_rc $? "docker-compose up"
+
+stepper "Docker-compose status:"
+docker-compose ps
